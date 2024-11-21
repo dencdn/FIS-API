99d91ef7fcd5e9e1a6e2e4115806648675e7d5af
den00333 <cordondinielle00333@gmail.com>
2024-11-21 09:34:51 +0800

    niafis

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const EditorOperatorRouter = require('./routes/editorOperatorRoutes')
 const app = express()
 
 app.use(cors({
-  origin: 'https://fis-railway-production.up.railway.app',
+  origin: 'https://niafis.com',
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true
