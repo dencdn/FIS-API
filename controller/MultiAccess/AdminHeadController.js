ec747d25b16e42552c0ba233b85d2f79045b3b39
den00333 <cordondinielle00333@gmail.com>
2025-02-11 20:42:44 +0800

    adminhead

--- controller/MultiAccess/AdminHeadController.js
+++ controller/MultiAccess/AdminHeadController.js
@@ -5,10 +5,12 @@ const axios = require('axios');
 const getForecastedValues = async(req, res) => {
     try{
       const awsAPI_URL = process.env.AWS_API_URL
+      // const awsAPI_URL = 'http://10.1.50.91:8000'
       const data = {
         steps: 12
       }
       const response = await axios.post(`${awsAPI_URL}/forecast`, data);
+     
       res.status(200).json(response.data)
     }catch(error){
       console.log('error getting forecasted values', error)
@@ -51,8 +53,20 @@ const sendTestExpense = async (req, res) => {
   }
 }
 
+const getRecords = async(req, res) => {
+  try {
+    const awsAPI_URL = process.env.AWS_API_URL
+    const response = await axios.get(`${awsAPI_URL}/records`);
+    res.status(200).json(response.data)
+  } catch(error) {
+    console.log('error sending records', error)
+    res.status(500)
+  }
+}
+
 module.exports = {
     getForecastedValues,
     getPercentageForMonth,
-    sendTestExpense
+    sendTestExpense,
+    getRecords
 }
--- routes/Admin_Head_routes.js
+++ routes/Admin_Head_routes.js
@@ -7,7 +7,8 @@ const setRole = require('../middleware/Role')
 //import from adminHeadCOntroller
 const {getForecastedValues,
        getPercentageForMonth,
-       sendTestExpense
+       sendTestExpense,
+       getRecords
       } = require('../controller/MultiAccess/AdminHeadController')
 
 admin_Head_Router.use(setRole([1,2]))
@@ -16,5 +17,6 @@ admin_Head_Router.use(setRole([1,2]))
 admin_Head_Router.get('/getForecastedValues', getForecastedValues)
 admin_Head_Router.post('/getPercentageForMonth', getPercentageForMonth)
 admin_Head_Router.post('/sendTestExpense', sendTestExpense)
+admin_Head_Router.get('/getRecords', getRecords)
 
 module.exports = admin_Head_Router;
